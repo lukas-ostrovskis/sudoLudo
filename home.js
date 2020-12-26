4cc09788495136ad72f931315e78803ded3af373
Martynas Krupskis <martynas@Martynass-MacBook-Pro.local>
2020-12-26 18:42:06 +0200

    added splash screen functionality
    splash

--- home.js
+++ home.js
@@ -1,2 +1,18 @@
 //@ts-check
 
+
+var show = function(){
+    document.getElementById("howTo").style.display = "block";
+    document.getElementById("howToButton").style.display = "block";
+    
+}
+
+var hide = function(){
+    document.getElementById("howTo").style.display = "none";
+    document.getElementById("howToButton").style.display = "none";
+    
+}
+
+document.querySelector(".btn1").addEventListener("click", show);
+document.querySelector(".hidden-btn").addEventListener("click", hide);
+
