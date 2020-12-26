8774238a5dc7ca0e446ff6a08e764fa84b243fc1
Martynas Krupskis <martynas@Martynass-MacBook-Pro.local>
2020-12-26 15:43:27 +0200

    js-script
    js-script

--- func.js
+++ func.js
@@ -1,6 +1,5 @@
 //@ts-check
 
-
 var diceModule = ( function(){
     var currentVal = 6;
 
@@ -13,3 +12,25 @@ var diceModule = ( function(){
         }
     }
 })();
+
+var playerOneModule = (function(nameInput) {
+
+    var score = 0;
+    var name = nameInput;
+    var figure1 = new Figure();
+    var figure2 = new Figure();
+    var figure3 = new Figure();
+    var figure4 = new Figure();
+    return{
+          incScore: function(){
+              score++;
+          },
+          getScore: function(){
+              return score;
+          }
+    }
+});
+
+// usage new (Player())();
+
+
