5399671b11d25fd9f3a6d610ba3ccd01059ed9b3
Martynas Krupskis <martynas@Martynass-MacBook-Pro.local>
2020-12-26 15:22:18 +0200

    js script
    js script

--- func.js
+++ func.js
@@ -1,1 +1,15 @@
 //@ts-check
+
+
+var diceModule = ( function(){
+    var currentVal = 6;
+
+    return {
+        currentValue: function(){
+            return currentVal;
+        },
+        rollDice: function(){
+            currentVal = Math.ceil(Math.random()*6);
+        }
+    }
+})();
